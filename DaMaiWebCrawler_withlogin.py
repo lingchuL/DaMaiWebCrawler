b4e70301e6e15da59634201b88e3c1f9840871f5
lingchuL <2938843848@qq.com>
2023-01-02 20:09:25 +0800

    Update DaMaiWebCrawler_withlogin.py
    优化检索按钮的方式 尽量使用class遍历 减少xpath匹配

--- DaMaiWebCrawler_withlogin.py
+++ DaMaiWebCrawler_withlogin.py
@@ -20,12 +20,12 @@ import time
 
 option = webdriver.ChromeOptions()
 
-option.add_argument(r'user-data-dir=C:\Users\PC\AppData\Local\Google\Chrome\User Data backup')
+option.add_argument(r'user-data-dir=C:\Users\wangj\AppData\Local\Google\Chrome\User Data backup')
 option.add_argument("--disable-gpu")
 option.add_argument("--no-sandbox")
 option.add_experimental_option('excludeSwitches', ['enable-automation'])
 option.add_argument(
-	r'user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"')
+	r'user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"')
 # option.add_argument(r"detach = true")
 option.add_argument("--disable-blink-features=AutomationControlled")
 
@@ -39,19 +39,19 @@ browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 		"""
 })
 
-url = "https://detail.damai.cn/item.htm?id=691288119897"
+url = "https://detail.damai.cn/item.htm?id=695973461503"
 testurl = "https://detail.damai.cn/item.htm?id=687315832580"
 
-browser.get(testurl)
+browser.get(url)
 
 browser.maximize_window()
 
 browser.implicitly_wait(15)
 
-if os.path.exists(r"E:\\damai_cookies.txt"):
+if os.path.exists(r"D:\\damai_cookies.txt"):
 	# 如果有cookies 直接登录
 
-	with open(r"E:\\damai_cookies.txt", "r", encoding="utf-8") as f:
+	with open(r"D:\\damai_cookies.txt", "r", encoding="utf-8") as f:
 		listCookies = json.loads(f.read())
 
 	for cookie in listCookies:
@@ -70,12 +70,16 @@ if os.path.exists(r"E:\\damai_cookies.txt"):
 else:
 	# 点击登录
 
-	ToLoginButton = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[1]/div[1]")
-	# ToLoginButton = browser.find_element(By.CLASS_NAME, "login-user show")
-	actions = ActionChains(browser)
-	actions.move_to_element(ToLoginButton)
-	actions.click()
-	actions.perform()
+	# ToLoginButton = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[1]/div[1]")
+	ToLoginButtons = browser.find_elements(By.CLASS_NAME, "login-user")
+	for button in ToLoginButtons:
+		if "登录" not in button.text:
+			continue
+		ToLoginButton = button
+		actions = ActionChains(browser)
+		actions.move_to_element(ToLoginButton)
+		actions.click()
+		actions.perform()
 
 # 检验是否跳转到了详情页面
 
@@ -88,17 +92,41 @@ print("成功登录")
 dictCookies = browser.get_cookies()
 jsonCookies = json.dumps(dictCookies)
 
-with open(r"E:\\damai_cookies.txt", "w") as f:
+with open(r"D:\\damai_cookies.txt", "w") as f:
 	f.write(jsonCookies)
 
-# 等待到时间
+# TODO 等待到时间
 
+# 选中城市
+CityListButtons = browser.find_elements(By.CLASS_NAME, "cityitem")
+for button in CityListButtons:
+	print()
+	if "dcity_1" not in button.get_attribute("data-spm"):
+		continue
+	CityButton = button
+	actions = ActionChains(browser)
+	actions.move_to_element(CityButton)
+	actions.click()
+	actions.perform()
+
+'''
+# 选中场次
+TimeButtons = browser.find_elements(By.CLASS_NAME, "select_right_list_item")
+for button in TimeButtons:
+	if "二" not in button.text:
+		continue
+	TimeButton = button
+	actions = ActionChains(browser)
+	actions.move_to_element(TimeButton)
+	actions.click()
+	actions.perform()
+'''
 
 # 选中vip票
 # VIPButton = browser.find_element_by_xpath("/html/body/div[3]/div/div[1]/div[1]/div/div[2]/div[3]/div[6]/div[2]/div/div[2]/div")
 SkuNameButtons = browser.find_elements(By.CLASS_NAME, "skuname")
 for button in SkuNameButtons:
-	if "228" not in button.text:
+	if "280" not in button.text:
 		continue
 	VIPButton = button
 	actions = ActionChains(browser)
@@ -106,75 +134,92 @@ for button in SkuNameButtons:
 	actions.click()
 	actions.perform()
 
-# 通过购买按钮判断是否刷新
-bHasRefresh = False
-while True:
-	BuyButton = browser.find_elements(By.CLASS_NAME, "buybtn")
-	for button in BuyButton:
-		if "购买" in button.text:
-			bHasRefresh = True
+try:
+	# 通过购买按钮判断是否刷新
+	bHasRefresh = False
+	while True:
+		BuyButton = browser.find_elements(By.CLASS_NAME, "buybtn")
+		for button in BuyButton:
+			if "购买" in button.text:
+				bHasRefresh = True
+				break
+
+		if bHasRefresh:
 			break
 
-	if bHasRefresh:
-		break
+	time.sleep(0.1)
 
-time.sleep(0.1)
+	# 检查登录状态
+	bHasRefreshLogin = False
+	while True:
+		UserNames = browser.find_elements(By.CLASS_NAME, "name-user")
+		for username in UserNames:
+			if "麦子41xL2" not in username.text:
+				time.sleep(0.001)
+			else:
+				bHasRefreshLogin = True
+				print("购买页面刷新后已成功再次登录！")
+				break
 
-# 检查登陆状态
-bHasRefreshLogin = False
-while True:
-	UserNames = browser.find_elements(By.CLASS_NAME, "name-user")
-	for username in UserNames:
-		if "麦子41xL2" not in username.text:
-			time.sleep(0.001)
+		if bHasRefreshLogin:
+			break
+
+	# 点击购买
+	bHasClickedBuy = False
+	while True:
+		BuyButton = browser.find_elements(By.CLASS_NAME, "buybtn")
+		for button in BuyButton:
+			if "购买" in button.text:
+				realBuyButton = button
+
+				actions = ActionChains(browser)
+				actions.move_to_element(realBuyButton)
+				actions.click()
+				actions.perform()
+
+				print("已点击BuyBtn")
+				bHasClickedBuy = True
+
+		if bHasClickedBuy:
+			break
 		else:
-			bHasRefreshLogin = True
-			print("购买页面刷新后已成功再次登录！")
+			time.sleep(0.00001)
+
+	# 点击添加观演人
+	visitorButtons = browser.find_elements(By.CLASS_NAME, "next-checkbox-label")
+	for button in visitorButtons:
+		if "王军懿" not in button.text:
+			continue
+		CheckVisitorButton = button
+		actions = ActionChains(browser)
+		actions.move_to_element(CheckVisitorButton)
+		actions.click()
+		actions.perform()
+
+	# 点击确认
+
+	bHasFoundConfirmButton = False
+	while True:
+		ConfirmButtons = browser.find_elements(By.CLASS_NAME, "next-btn")
+		for button in ConfirmButtons:
+			if "提交订单" in button.text:
+				bHasFoundConfirmButton = True
+				RealConfirmButton = button
+
+				actions = ActionChains(browser)
+				actions.move_to_element(RealConfirmButton)
+				actions.click()
+				actions.perform()
+
+				break
+
+		if bHasFoundConfirmButton:
 			break
 
-	if bHasRefreshLogin:
-		break
-
-# 点击购买
-bHasClickedBuy = False
-while True:
-	BuyButton = browser.find_elements(By.CLASS_NAME, "buybtn")
-	for button in BuyButton:
-		if "购买" in button.text:
-			realBuyButton = button
-
-			actions = ActionChains(browser)
-			actions.move_to_element(realBuyButton)
-			actions.click()
-			actions.perform()
-
-			print("已点击BuyBtn")
-			bHasClickedBuy = True
-
-	if bHasClickedBuy:
-		break
-	else:
-		time.sleep(0.00001)
-
-# 点击确认
-
-bHasFoundConfirmButton = False
-while True:
-	ConfirmButtons = browser.find_elements(By.CLASS_NAME, "next-btn")
-	for button in ConfirmButtons:
-		if "提交订单" in button.text:
-			bHasFoundConfirmButton = True
-			RealConfirmButton = button
-
-			actions = ActionChains(browser)
-			actions.move_to_element(RealConfirmButton)
-			actions.click()
-			actions.perform()
-
-			break
-
-	if bHasFoundConfirmButton:
-		break
+except:
+	print("登录后有错误发生")
+finally:
+	pass
 
 
 time.sleep(60)
